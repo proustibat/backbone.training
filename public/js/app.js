fa9b806f7f39870fddccfffc145268cb10c7d7e0
yamsellem <amsellem.yves@gmail.com>
2014-10-19 11:13:05 +0200

    fix

--- public/js/app.js
+++ public/js/app.js
@@ -75,7 +75,68 @@ $(function() {
 		}, 2000)
 	});
 
-	var musicians = new Musicians();
-	var musiciansView = new MusiciansView({el: '.js-main', collection: musicians})
-	var notificationsView = new NotificationsView({el: '.js-notification', collection: musicians});
+	var NavView = Backbone.View.extend({
+		el: 'nav',
+		events: {
+			'click .js-creation': 'create',
+			'click .js-login': 'login',
+			'click h3': 'home'
+		},
+		home: function(e) {
+			this.navigate(e, '');
+		},
+		create: function(e) {
+			this.navigate(e, 'creation');
+		},
+		login: function(e) {
+			this.navigate(e, 'login');
+		},
+		navigate: function(e, route) {
+			e.preventDefault();
+			Backbone.history.navigate(route, {trigger: true});
+		}
+	});
+
+	var LayoutView = Backbone.View.extend({
+		el: '#layout-element',
+		template: Handlebars.compile($("#layout-template").html()),
+		render: function(type) {
+			this.$el.html(this.template());
+
+			var musicians = new Musicians();
+			var notificationsView = new NotificationsView({el: '.js-notification', collection: musicians});
+
+			switch(type) {
+			case 'home': 
+				new MusiciansView({el: '.js-main', collection: musicians});
+			break;
+			case 'creation':
+			break;
+			case 'login':
+			break;
+			}
+		}
+	});
+
+	var Router = Backbone.Router.extend({
+		routes: {
+			"creation": "creation",
+			"login": "login",
+			"*path": "home"
+		},
+		creation: function() {
+			new LayoutView().render('creation');
+		},
+		login: function() {
+			new LayoutView().render('login');
+		},
+		home: function() {
+			new LayoutView().render('home');
+		}
+	});	
+
+	new NavView();
+	new Router();
+
+	Backbone.history.start();
 });
