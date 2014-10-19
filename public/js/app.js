08eef37667632b9a5b5e32776b2007a57012b51b
yamsellem <amsellem.yves@gmail.com>
2014-10-19 10:35:41 +0200

    fix

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,35 @@
 "use strict";
 
 $(function() {
-	var MusiciansView = Backbone.View.extend({
+	var Musicians = Backbone.Collection.extend({
+		url: '/musician',
+		model: Backbone.Model.extend({
+			urlRoot: '/musician'
+		})
+	});
+
+	var MusicianView = Backbone.View.extend({
 		template: Handlebars.compile($("#musician-template").html()),
 		initialize: function() {
-			this.$el.html(this.template());
+			this.$el.html(this.template(this.model.toJSON()));
 		}
 	});
 
-	var musiciansView = new MusiciansView({el: '.js-main'})
+	var MusiciansView = Backbone.View.extend({
+		initialize: function() {
+			this.collection.fetch();
+			this.listenTo(this.collection, 'sync', this.render);
+		},
+		render: function() {
+			this.$el.html('');
+			
+			this.collection.each(function(model, index) {
+				var child = new MusicianView({model: model});
+				this.$el.append(child.el);
+			}, this);
+		}
+	});
+
+	var musicians = new Musicians();
+	var musiciansView = new MusiciansView({el: '.js-main', collection: musicians})
 });
