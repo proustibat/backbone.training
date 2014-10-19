f9c94a713be7f5d2b0825d8d8a0eef6293f98f7f
yamsellem <amsellem.yves@gmail.com>
2014-10-19 10:25:50 +0200

    fix

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 $(function() {
-	var MusicianView = Backbone.View.extend({
+	var MusiciansView = Backbone.View.extend({
 		template: Handlebars.compile($("#musician-template").html()),
 		initialize: function() {
 			this.$el.html(this.template());
