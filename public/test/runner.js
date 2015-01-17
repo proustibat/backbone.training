03a02d09b307a6141103a621b23c8a82d3488a3e
yamsellem <amsellem.yves@gmail.com>
2015-01-17 11:46:44 +0100

    fix

--- public/test/runner.js
+++ public/test/runner.js
@@ -1,31 +1,21 @@
 require.config({
-    baseUrl: '../js',
-    paths: {
-        jquery: '../bower_components/jquery/dist/jquery',
-        underscore: '../bower_components/underscore/underscore',
-        backbone: '../bower_components/backbone/backbone',
-        handlebars: '../bower_components/handlebars/handlebars',
-        text: '../bower_components/requirejs-text/text',
-
-        mocha: '../bower_components/mocha/mocha',
-        chai: '../bower_components/chai/chai'
-    },
-    shim: {
-        backbone: { deps: ['jquery', 'underscore'], exports: 'Backbone' },
-        underscore: { exports: '_' },
-        handlebars: { exports: 'Handlebars'},
-        
-        mocha: { exports: 'mocha' }
-    }
+	baseUrl: '../js',
+	paths: {
+			jquery: '../bower_components/jquery/dist/jquery',
+			underscore: '../bower_components/underscore/underscore',
+			backbone: '../bower_components/backbone/backbone',
+			handlebars: '../bower_components/handlebars/handlebars',
+			text: '../bower_components/requirejs-text/text'
+	},
+	shim: {
+			backbone: { deps: ['jquery', 'underscore'], exports: 'Backbone' },
+			underscore: { exports: '_' },
+			handlebars: { exports: 'Handlebars'}
+	}
 });
  
-define(function(require) {
-  var mocha = require('mocha');
-  mocha.setup('bdd');
- 
-  require([
-    'musicians/MusiciansTest.js',
-  ], function(require) {
-    mocha.run();
-  });
+require([
+	'musicians/MusiciansTest.js',
+], function(require) {
+	mocha.run();
 });
