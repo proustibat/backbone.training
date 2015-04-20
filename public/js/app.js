9bc455db10a6e8bca10b809e55e03721c530a7bd
Proustibat <jennifer.proust@gmail.com>
2015-04-20 15:01:50 +0200

    step 2

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,60 @@
 "use strict";
 
-// let the aventure begin!
+var MusiciansViews = Backbone.View.extend({
+    events: {
+
+    },
+    initialize: function() {
+        console.log('MusiciansViews.initialize');
+        // this.template = $("#musicians-list-template").html();
+        this.template = Handlebars.compile($("#musicians-list-template").html());
+
+        // 1. Ecouteurs
+        // this.listenTo(this.collection, 'sync', this.render);
+        // this.collection.fetch();
+
+        // 2. Sauvegarde le contexte
+        // var self = this;
+        // this.collection.fetch().done(function(){
+        //     self.render();
+        // });
+
+        // 3. bind contexte (incompatible anciens navigateurs)
+        // this.collection.fetch().done(this.render.bind(this));
+
+        // 4. bind contexte via underscore
+        this.collection.fetch().done(_.bind(this.render,this));
+    },
+
+    render: function() {
+        console.log('MusiciansViews.render');
+        // console.log('collection data : ', this.collection.toJSON());
+        this.$el.html(this.template(this.collection.toJSON()));
+    }
+});
+
+var MusicianCollection = Backbone.Collection.extend({
+    url: "/musician",
+    initialize: function() {
+        console.log('MusicianCollection.initialize');
+    },
+    render: function() {
+
+    }
+});
+
+
+$(document).ready(function() {
+
+    var musicianCollection = new MusicianCollection();
+
+//
+    var musicianViews = new MusiciansViews({
+        el: ".js-musicians-list",
+        collection: musicianCollection
+    });
+
+    // musicianCollection.fetch().done(function(){
+    //     console.log('List found ');
+    // });
+});
